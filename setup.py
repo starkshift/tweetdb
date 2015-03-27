122bc1f1aa926ed859e377c28feaa780ac13645b
Russell Miller <nunuva@bizness.com>
2015-03-26 22:46:32 -0400

    Apparently storing images as a database blob is a terrible idea.  Who knew?  Implemeted image storage in the file system.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tweetdb',
+      version='0.1',
+      description='Some code for working with twitter data.',
+      url='https://github.com/starkshift/tweetdb',
+      author='Russell Miller',
+      author_email='',
+      license='MIT',
+      packages=['tweetdb'],
+      zip_safe=False)
